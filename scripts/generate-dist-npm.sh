137f7c40b75b6e9f7bd52ed7586a31ca54230705
wandeber <berthewanderer@gmail.com>
2021-05-24 02:12:30 +0200

    NPM registry.

---
+++ scripts/generate-dist-npm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p dist-npm
+cp -rf dist dist-npm/
+cp -rf other-dependencies dist-npm/
+cp -f package*.json dist-npm/
+cp -f .npmrc dist-npm/
+cp -f LICENSE dist-npm/
+cp -f README.md dist-npm/
+
+echo "\n//registry.npmjs.org/:_authToken=\${NPM_REGISTRY_TOKEN}" >> dist-npm/.npmrc
+sed -i '' 's!@wandeber\/agnese!agnese!g' dist-npm/package*.json
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 // Extensions .js in order to support browsers.
-export {default as Agnese} from "./Agnese.js";
+import Agnese from "./Agnese.js";
+
+export default Agnese;
+export {Agnese};
 export {FieldType} from "./MapTypes.js";
