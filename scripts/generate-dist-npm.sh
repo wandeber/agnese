835e0d372f4f79723d73e64dd659703f6c6d5966
wandeber <berthewanderer@gmail.com>
2021-05-24 02:59:00 +0200

    Fix CI.

--- scripts/generate-dist-npm.sh
+++ scripts/generate-dist-npm.sh
@@ -3,10 +3,13 @@
 mkdir -p dist-npm
 cp -rf dist dist-npm/
 cp -rf other-dependencies dist-npm/
-cp -f package*.json dist-npm/
+cp -f package.json dist-npm/
+cp -f package-lock.json dist-npm/
 cp -f .npmrc dist-npm/
 cp -f LICENSE dist-npm/
 cp -f README.md dist-npm/
 
 echo "\n//registry.npmjs.org/:_authToken=\${NPM_REGISTRY_TOKEN}" >> dist-npm/.npmrc
-sed -i 's!@wandeber\/agnese!agnese!g' dist-npm/package*.json
+sed -i.bak 's!@wandeber\/agnese!agnese!g' dist-npm/package*.json
+
+rm -f dist-npm/package*.json.bak
