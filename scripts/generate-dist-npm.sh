fa0747b603929eceaa258b88a3f73262abc7aa49
wandeber <berthewanderer@gmail.com>
2022-07-13 19:28:56 +0200

    Add schema directory during build.

--- scripts/generate-dist-npm.sh
+++ scripts/generate-dist-npm.sh
@@ -3,6 +3,7 @@
 mkdir -p dist-npm
 cp -rf dist dist-npm/
 cp -rf other-dependencies dist-npm/
+cp -rf schema dist-npm/
 cp -f package*.json dist-npm/
 cp -f .npmrc dist-npm/
 cp -f LICENSE dist-npm/
