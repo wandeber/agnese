d63adc914647050c9d4b598b9593c71d2152600c
Ber A. Siverio <berthewanderer@gmail.com>
2025-11-16 19:37:18 +0100

    build: migrate to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  splitting: false,
+  target: "node18",
+  outDir: "dist",
+  tsconfig: "./tsconfig.base.json",
+  skipNodeModulesBundle: true,
+  outExtension({format}) {
+    return {
+      js: format === "esm" ? ".mjs" : ".js"
+    };
+  }
+});
